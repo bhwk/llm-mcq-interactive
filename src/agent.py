720715417fe534a72f74481f81e21064c89ef75d
bhwk <bryanhawaikit@gmail.com>
2025-02-26 15:24:52 +0800

    fix correct answer return

--- src/agent.py
+++ src/agent.py
@@ -16,9 +16,8 @@ def create_agent(
     global answer_map
     if current_question is not None:
         question = current_question["question"]
-        correct_answer = current_question.get(
-            str(answer_map.get(current_question["cop"]))
-        )  # type: ignore
+        correct_option = current_question["cop"]
+        correct_answer = current_question.get(str(correct_option))  # type: ignore
         explanation = current_question["exp"]
         shared_variables = {
             "question": question,  # type: ignore
